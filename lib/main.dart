78f5f558d18cfce23115a59e496360f0679e7086
Saeed <sajalingo@gmail.com>
2025-09-24 11:02:49 +0100

    added MyHomePage stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
       ),
       home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() =>
+      _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('A Reader')),
+      body: Column(children: [Text('Hello!!!')]),
     );
   }
 }
