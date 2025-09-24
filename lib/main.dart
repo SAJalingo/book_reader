f7fe9f7df215ca4aced247d0c009d9724db1964e
Saeed <sajalingo@gmail.com>
2025-09-24 11:29:13 +0100

    added background color to appbar

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('A Reader')),
+      appBar: AppBar(
+        backgroundColor: Theme.of(
+          context,
+        ).colorScheme.inversePrimary,
+        title: Text('A Reader'),
+      ),
       body: Column(children: [Text('Hello!!!')]),
     );
   }
