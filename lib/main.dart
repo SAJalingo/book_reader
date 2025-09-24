a8cdd92c9fc5a34daa9940d6d59dc95d1d41dcf9
Saeed <sajalingo@gmail.com>
2025-09-24 11:32:12 +0100

    changed the material theme color from purple to orange accent

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
+          seedColor: Colors.orangeAccent,
         ),
       ),
       home: const MyHomePage(),
